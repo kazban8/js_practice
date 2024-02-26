f68d0264edb2a650a3505d2a533edede30bf91e6
kazban8 <viktor.kazban@gmail.com>
2024-02-26 20:35:08 +0200

    dante commit

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('red test'));
+console.log(chalk.yellow('yellow test'));
+console.log(chalk.gray('gray test'))
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,7 @@
+let name = "Dante";
+let age = 88;
+let isSunny = false;
+
+console.log("Aligieri "+name)
+console.log(age)
+console.log(isSunny)
